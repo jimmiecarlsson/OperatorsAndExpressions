8edd7d1554ad345213a88aef697fb261f0692c68
jimmiecarlsson <jimpa.c66@gmail.com>
2025-07-29 11:29:05 +0200

    Done execise 1

--- OperatorsAndExpressions/Program.cs
+++ OperatorsAndExpressions/Program.cs
@@ -1,3 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+//Exercise 1
+
+Console.WriteLine("Hello" + " " + "World!"); // Hello World
+
+Console.WriteLine(2 + 4); // 6
+
+Console.WriteLine("2" + "4"); // 24
+
+Console.WriteLine(2 + "4"); // 24
+
+Console.WriteLine("2" + 4 + 7); // 247
+
+Console.WriteLine(3 + 5 + "=" + 3 + 5); // 8=35
+
+Console.WriteLine(6 + 2 + "3"); // 83
+
+Console.WriteLine("Two" + "Three"); // TwoThree
+
+Console.WriteLine(1 + 2 + "Three"); // 3Three
+
+Console.WriteLine(4>2); // true
+
+Console.WriteLine(34 < 32 + 4); // true (+ går före <)
+
+Console.WriteLine(33 != 33); // false
+
+Console.WriteLine(34 - 2 == 36); // false
+
+Console.WriteLine(34 + 2 != 36); // false (+ går före !=, 36 != 36)
+
+Console.WriteLine(34 + 2 >= 36); // true
+
+Console.WriteLine(34-2 > 36); // false
+
+Console.WriteLine(4 == 4 ? "One": "Two"); // true
+
+//Exercise 2
+
+//Console.WriteLine(); //
+
+//Console.WriteLine(); //
+
+//Console.WriteLine(); //
+
+//Console.WriteLine(); //
+
+//Console.WriteLine(); //
+
+//Console.WriteLine(); //
